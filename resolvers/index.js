effc9b48af711f276abbe6d7f362f7b24b93662a
Noor ul haq <noor_ul_haq96@hotmail.com>
2017-08-13 23:15:35 +0500

    add updateTodo mutation resolver to index.js

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,6 +3,7 @@ const todos = require('./todos-resolver');
 const user = require('./user-resolver');
 const createTodo = require('./createTodo-mutation');
 const deleteTodos = require('./deleteTodos-mutation');
+const updateTodo = require('./updateTodo-mutation');
 const Todo = require('./todo-type-resolver');
 const User = require('./user-type-resolver');
 const Status = require('./status-type-resolver');
@@ -15,7 +16,8 @@ module.exports = {
 	},
 	Mutation: {
 		createTodo,
-		deleteTodos
+		deleteTodos,
+		updateTodo
 	},
 	Todo,
 	User,
